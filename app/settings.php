ee315efac2cf0856f0dcb0c429eb3c61beb5df06
Antonino <45453064+AntoninoM90@users.noreply.github.com>
2024-06-02 00:17:50 +0200

    Updated the Doctrine ORM metadata cache on dependencies and settings

--- app/settings.php
+++ app/settings.php
@@ -27,10 +27,6 @@ return function (ContainerBuilder $containerBuilder) {
                     // if true, metadata caching is forcefully disabled
                     'dev_mode' => true,
 
-                    // path where the compiled metadata info will be cached
-                    // make sure the path exists and it is writable
-                    'cache_dir' => __DIR__ . '/../var/doctrine',
-
                     // you should add any other path containing annotated entity classes
                     'metadata_dirs' => [__DIR__ . '/../src/Domain'],
 
